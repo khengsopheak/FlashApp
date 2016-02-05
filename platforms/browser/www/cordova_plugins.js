6239439853a49a24d821826a16811517907bcf89
keasorn <kea3rrd@gmial.com>
2016-02-05 08:23:50 +0700

    up-to-dat

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
+        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "clobbers": [
+            "plugin.google.maps"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.0"
+    "cordova-plugin-whitelist": "1.2.0",
+    "plugin.google.maps": "1.3.9"
 }
 // BOTTOM OF METADATA
 });
--- www/js/app/action/userposts.js
+++ www/js/app/action/userposts.js
@@ -1,11 +1,11 @@
 $('form').submit(function () {
     var postData = $(this).serialize();
-
+console.log("add new data");
     $.ajax({
         type: 'POST',
         data: postData,    
         contentType: "application/x-www-form-urlencoded;charset=UTF-8",
-        url: 'http://espr.website/any/userposts.php',
+        url: 'http://espr.website/any/register.php',
         success: function (data) {
             console.log(data);
             alert('Your comment was successfully added');
@@ -15,6 +15,5 @@ $('form').submit(function () {
             alert('There was an error adding your comment');
         }
     });
-
     return false;
 });
